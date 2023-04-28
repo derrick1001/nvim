191da09ad511351b79e6a9a11dcae034499f5571
derrick <derrick.ishman@gmail.com>
2023-04-28 11:47:09 -0500

    Test

--- lua/lshadows/remap.lua
+++ lua/lshadows/remap.lua
@@ -39,6 +39,8 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- comment
+
 
 
 
