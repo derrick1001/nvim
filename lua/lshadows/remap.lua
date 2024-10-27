e9dbdace35179e2a20783df4d7666631995138c4
derrick <twohackerauth@proton.me>
2024-10-27 01:23:19 -0500

    Hello

--- lua/lshadows/remap.lua
+++ lua/lshadows/remap.lua
@@ -34,6 +34,6 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r>/\>/<C-r>//gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>S", [[:%s/\<<C-r>/\>/<C-r>//gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod u+x %<CR>", { silent = true })
